0f047627da4f343a72c7e1b73417b9abc64f9411
barrerya <47180948+barrerya@users.noreply.github.com>
2021-04-13 19:09:16 -0400

    Removed placeholder page data on servlets

--- src/java/Servlets/RegistrationServlet.java
+++ src/java/Servlets/RegistrationServlet.java
@@ -33,16 +33,7 @@ public class RegistrationServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RegistrationServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RegistrationServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            
         }
     }
 
